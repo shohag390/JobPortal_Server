177735225aa9e81bd0d4cb86107e56d9864b1444
Md Shohag Ali <shohag.webdev@gmail.com>
2026-08-26 21:11:15 +0600

    Jobs Post Api Create

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    // MongoDB Collection
+    const jobsCollections = client.db("jobPortal").collection("jobs");
+
+    // Jobs Related Api
+    // Jobs Post
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollections.insertOne(newJob);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +44,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  resizeBy.send("Api Working");
+  res.send({
+    message: "Api Working",
+  });
 });
 
 app.listen(port, () => {

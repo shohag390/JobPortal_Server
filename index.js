34476428deab2e368529d79e673979573e3e83bd
Md Shohag Ali <shohag.webdev@gmail.com>
2026-08-28 17:13:46 +0600

    Comment Some Code

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     // MongoDB Collection
     const jobsCollections = client.db("jobPortal").collection("jobs");
     const applicationCollections = client
